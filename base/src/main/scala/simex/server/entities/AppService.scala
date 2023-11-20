52e40b5978de696392fe622f4253d42794fc07cd
TheDiscProg <132068665+TheDiscProg@users.noreply.github.com>
2023-11-20 09:12:57 +0000

    Initial commit

---
+++ base/src/main/scala/simex/server/entities/AppService.scala
@@ -0,0 +1,8 @@
+package simex.server.entities
+
+import org.http4s.server.Server
+
+// Add additional properties as required
+case class AppService(
+    server: Server
+)
---
+++ project/Dependencies.scala
@@ -0,0 +1,15 @@
+import sbt._
+
+object Dependencies {
+
+  lazy val all = Seq(
+    "Shareprice" %% "shareprice-config" % "0.8.0",
+    "org.http4s" %% "http4s-dsl" % "0.23.18",
+    "org.http4s" %% "http4s-ember-server" % "0.23.18",
+    "org.http4s" %% "http4s-ember-client" % "0.23.18",
+    "org.http4s" %% "http4s-circe" % "0.23.18",
+    "org.typelevel" %% "munit-cats-effect-2" % "1.0.7" % Test,
+    "org.scalatest" %% "scalatest" % "3.2.15" % Test,
+    "org.scalatestplus" %% "mockito-4-6" % "3.2.15.0" % Test
+  )
+}
